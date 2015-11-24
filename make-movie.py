853a723c3b7ef4d8e62779618630958119b1b2e2
Stefan Contractor <Stefan@MacBook-Pro.local>
2015-11-24 13:33:22 +1100

    Basic main function

---
+++ make-movie.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+"""
+Comments here about the script
+"""
+
+def main():
+	print "Hello World!"
+	return True
+
+if __name__=="__main__":
+	sys.exit(main())
+
+
